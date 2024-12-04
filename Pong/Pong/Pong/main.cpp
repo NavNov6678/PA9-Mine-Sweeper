0c8e25d8177530fb696e67cce5eeb9b5d82e9b53
getlz <mauricio34kmo@gmail.com>
2024-12-03 20:03:43 -0800

    Updated Pong
    idk why but the code would run so i just added it again

--- Pong/Pong/Pong/main.cpp
+++ Pong/Pong/Pong/main.cpp
@@ -4,24 +4,36 @@
 // History: Started from the SFML tutorial code that draws
 //          a circle in the window.
 // 
+//          11/20/24 - Implemented some collision detection and audio.
+// 
 //          11/19/24 - Finished declaring class Paddle. Introduced some basic
 //                     paddle movements.
 //
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio/Music.hpp>
 
 #include "Paddle.hpp"
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
-	sf::CircleShape shape(100.f);
+	
 	Paddle p1Paddle (sf::Vector2f(30, 100), sf::Vector2f(0,0), sf::Color::Magenta), 
 		p2Paddle(sf::Vector2f(window.getSize().x/20, window.getSize().y/5), 
 			sf::Vector2f(window.getSize().x-p1Paddle.getSize().x, 0), 
 			sf::Color::Magenta);
+	sf::CircleShape shape(window.getSize().x/20);
 
 	shape.setFillColor(sf::Color::Green);
+	shape.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+
+	int direction = 1;
+
+	sf::Music mus;
+
+	mus.openFromFile("orchestral.ogg");
+	mus.play();
 
 	while (window.isOpen())
 	{
@@ -32,18 +44,32 @@ int main()
 				window.close();
 		}
 
-		window.clear();
-		window.draw(shape);
-		window.draw(p1Paddle);
-		window.draw(p2Paddle);
-		window.display();
-
-		//p1Paddle.getGlobalBounds().intersects(shape.getGlobalBounds());
+		shape.move(direction * .01, 0);
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 		{
 			p1Paddle.move(0, .1);
 		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		{
+			p2Paddle.move(0, .1);
+		}
+
+		if (p1Paddle.getGlobalBounds().intersects(shape.getGlobalBounds()))
+		{
+			direction *= -1;
+		}
+		if (p2Paddle.getGlobalBounds().intersects(shape.getGlobalBounds()))
+		{
+			direction *= -1;
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.draw(p1Paddle);
+		window.draw(p2Paddle);
+		window.display();
+		
 
 
 	}
